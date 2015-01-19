a0ba43d0c9743098e7ecd3deedeeafddbdaf52d8
Aniket Bochare <aniket6666@gmail.com>
2015-01-18 20:30:59 -0600

    Added 3 sorting algos
    Insertion, selection and bubble.

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class BubbleSort {
+	
+public static void BubbleSort(int[] intArray)	{
+	int n = intArray.length;
+    int temp = 0;
+   
+    for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                   
+                    if(intArray[j-1] > intArray[j]){
+                            //swap the elements!
+                            temp = intArray[j-1];
+                            intArray[j-1] = intArray[j];
+                            intArray[j] = temp;
+                    }
+                   
+            }
+    }
+
+}
+
+}
---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,16 @@
+package Sorting;
+
+public class InsertionSort {
+
+	public static void insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > current) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = current;
+        }
+	}
+}
